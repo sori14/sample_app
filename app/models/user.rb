512edb2329e63c1ee41b5f7d5537a26ba4013835
sori14 <sori.juggling@gmail.com>
2019-04-07 19:45:14 +0900

    Finish ch14

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
-  validates :name, presence: true, length: {maximum: 50}
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: 'active_relationships', source: 'followed'
+  has_many :followers, through: 'passive_relationships', source: 'follower'
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
-
+  validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX},
@@ -73,7 +76,24 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?", self.id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id",
+                    user_id: self.id)
+  end
+
+  # ユーザをフォローする
+  def follow(other_user)
+    self.active_relationships.create(followed_id: other_user.id)
+  end
+
+  # ユーザを解除する
+  def unfollow(other_user)
+    self.active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザがフォローしていたら、trueを返す
+  def following?(other_user)
+    self.following.include?(other_user)
   end
 
   private
@@ -88,5 +108,4 @@ class User < ApplicationRecord
       # ハッシュ化する
       self.activation_digest = User.digest(self.activation_token)
     end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,19 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end
 
+# マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content)}
 end
+
+# リレーションシップ
+users = User.all
+# 最初のユーザの関係だけ作る
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| user.follow(user) }
+
